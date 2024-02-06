e018875cbd6742d527d29c2aa68359c8f0dd0d75
Yyuichiii <ritikmehradec@gmail.com>
2024-02-06 16:27:03 +0530

    improved the view and urls

--- Components/urls.py
+++ Components/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('passive/<str:type>/', views.passive,name='passive'),
-    path('active/<str:type>/', views.active,name='active'),
-    path('Electromechanical/<str:type>/', views.electromechanical,name='Electromechanical'),
-    path('Display/<str:type>/', views.display,name='Display'),
-    path('Sensor/<str:type>/', views.sensor,name='Sensor'),
+    path('<str:Category>/<str:type>/',views.Components_view,name='Components')
 ]
--- Components/views.py
+++ Components/views.py
@@ -1,48 +1,11 @@
 from django.shortcuts import render,redirect
-from .models import Passive,Active,Electromechanical,Display,Sensor
+from .models import Component
 
-# Create your views here.
-def passive(request,type):
-    try:
-        component=Passive.objects.get(type=type)
-    
-        return render(request,'Components/Passive.html',{'component':component})
-    
-    except:
-        return redirect('home')
-    
 
-def active(request,type):
+def Components_view(request,Category,type):
     try:
-        component=Active.objects.get(type=type)
-    
-        return render(request,'Components/Active.html',{'component':component})
-    
-    except:
-        return redirect('home')
-    
-
-def electromechanical(request,type):
-    try:
-        component=Electromechanical.objects.get(type=type)   
-        return render(request,'Components/Electromechanical.html',{'component':component})
-    
-    except:
-        return redirect('home')
-    
-def display(request,type):
-    try:
-        component=Display.objects.get(type=type)   
-        return render(request,'Components/Electromechanical.html',{'component':component})
-    
-    except:
-        return redirect('home')
-    
-       
-def sensor(request,type):
-    try:
-        component=Sensor.objects.get(type=type)   
-        return render(request,'Components/Electromechanical.html',{'component':component})
+        component=Component.objects.get(Type=type)  
+        return render(request,'Components/Components_content.html',{'component':component})
     
     except:
         return redirect('home')
